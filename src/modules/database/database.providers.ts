e591684b745dce97971cd68103657a2fa218e577
Matheus Costa <matheusluciox@gmail.com>
2019-06-12 10:21:04 -0300

    ajustes package json

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -5,9 +5,7 @@ export const databaseProviders = [
     provide: 'DbConnectionToken',
     useFactory: async (): Promise<mongoose.Connection> => {
       (mongoose as any).Promise = global.Promise;
-      return await mongoose.connect('mongodb://usuariosagencias:agenciasusuarios1@ds141623.mlab.com:41623/docker-usuarios-agencias', {
-        useMongoClient: true
-      });
+      return await mongoose.connect('mongodb://usuariosagencias:agenciasusuarios1@ds141623.mlab.com:41623/docker-usuarios-agencias');
     }
   }
 ];
